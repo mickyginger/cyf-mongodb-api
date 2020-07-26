1439fb67b7fa32fb6aedc626d4c9de792740eb19
byohannes <42332389+byohannes@users.noreply.github.com>
2020-07-26 15:09:01 +0100

    get all movies

--- server.js
+++ server.js
@@ -1,34 +1,46 @@
-const express = require('express');
-const mongodb = require('mongodb');
-const uri = 'mongodb+srv://<username>:<password>@<domain>';
-const mongoOptions = { useUnifiedTopology: true };
-const client = new mongodb.MongoClient(uri, mongoOptions);
+const express = require('express')
+const mongodb = require('mongodb')
+const dotenv = require('dotenv')
+const mongoOptions = {useUnifiedTopology: true}
+dotenv.config()
+const uri = process.env.DATABASE_URI
+const client = new mongodb.MongoClient(uri, mongoOptions)
 
-const app = express();
-app.use(express.json());
+const app = express()
+app.use(express.json())
 
 client.connect(function () {
-  const db = client.db('cinema');
+  const db = client.db('mongo-week3')
 
   app.get('/films', function (request, response) {
-    response.send('Retrieve all the films');
-  });
+    const collection = db.collection('movies')
+    collection.find().toArray((error, movies) => {
+      if (error) {
+        console.log(error)
+        response.status(500).send(error)
+      } else {
+        response.status(200).send(movies)
+      }
+    })
+  })
 
   app.get('/films/:id', function (request, response) {
-    response.send('Retrieve one film');
-  });
+    response.send('Retrieve one film')
+  })
 
   app.post('/films', function (request, response) {
-    response.send('Create a film');
-  });
+    response.send('Create a film')
+  })
 
   app.put('/films/:id', function (request, response) {
-    response.send('Update one film');
-  });
+    response.send('Update one film')
+  })
 
   app.delete('/films/:id', function (request, response) {
-    response.send('Delete one film');
-  });
+    response.send('Delete one film')
+  })
 
-  app.listen(3000);
-});
+  app.listen(3000, () => {
+    console.log(`Server running and listening`)
+  })
+})
